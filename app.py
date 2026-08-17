d4412d63aebce5b60542e7d5772e4704536ec374
SilvanaJimenez <melissa.18.11.98@gmail.com>
2026-08-16 22:59:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 
-st.image("Python_logo.png")
+
 
 
 st.title("Especialización Python for Analytics")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Silvana Jiménez")
+
+st.image("Python_logo.png",width = 50)
 
 
 
